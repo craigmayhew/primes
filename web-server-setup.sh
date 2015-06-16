0b5113b3a72d9b5fee01e3a068e47aae5f9d39ca
Craig Mayhew <craig@mayhew.io>
2015-06-16 20:50:58 +0100

    Added more notes and service now turns on

--- web-server-setup.sh
+++ web-server-setup.sh
@@ -1,5 +1,13 @@
+#this script has only beet tested on an Amazon Linux Machine Image
+
 #update yum packages without asking
 sudo yum update -y
 
 #install apache, php 5.6 and the mysql connector
 sudo yum install -y httpd24 php56 php56-mysqlnd
+
+#now if you want to turn the service on
+sudo service httpd start
+
+#and your webfiles should go here
+#/var/www
