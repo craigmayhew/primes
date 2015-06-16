b2e2ddbc3e04b703eb7cf7ef7625d250ea8cfc20
Craig Mayhew <craig@mayhew.io>
2015-06-16 20:57:39 +0100

    Added some security by user

--- web-server-setup.sh
+++ web-server-setup.sh
@@ -11,3 +11,11 @@ sudo service httpd start
 
 #and your webfiles should go here
 #/var/www
+
+#user based filesystem security
+sudo groupadd www
+sudo usermod -a -G www ec2-user
+sudo chown -R root:www /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} +
+find /var/www -type f -exec sudo chmod 0664 {} +
