bf7c2b51c3a11548b7566fd59b9e2be6b6541929
craigmayhew <craig@mayhew.io>
2015-06-18 22:41:28 +0100

    Updated the S3 code to save workunit results to S3

--- classes/pages/wuresult.php
+++ classes/pages/wuresult.php
@@ -5,6 +5,24 @@ class wuresult extends \pages{
   public function getContent(){
     $postedJSON = file_get_contents("php://input");
 
+    //json decode so we can access the id for the filename
+    $posted = json_decode($postedJSON);
+
+    //aws library
+    $aws = new \awsS3();
+
+    //create a uniquely named temp file
+    $filename = '/tmp/'.sha1(rand().time().$posted->id);
+
+    //save json to disk
+    file_put_contents($filename,$postedJSON);
+
+    //write file to s3
+    $return   = $aws->saveFileToS3($filename,$posted->id);
+    
+    //delete temporary file
+    unlink($filename);
+
     return '';
   }
 }
