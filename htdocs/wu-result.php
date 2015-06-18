e38219d312ba333d12686260641244be35b57f8d
craigmayhew <craig@mayhew.io>
2015-06-18 09:26:32 +0100

    Added lines to pull in autoloader etc

--- htdocs/wu-result.php
+++ htdocs/wu-result.php
@@ -1,4 +1,10 @@
 <?php
+date_default_timezone_set('Europe/London');
+ini_set('memory_limit','64M');
+require_once('autoLoader.php');
+require '../vendor/autoload.php';
+
+
 $postedJSON = file_get_contents("php://input");
 
 
