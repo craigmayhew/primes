fc01eea865e101a300440bba30b61469e9addd89
craigmayhew <craig@mayhew.io>
2015-06-18 09:26:09 +0100

    Added config file for aws

---
+++ config/aws.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+  // Bootstrap the configuration file with AWS specific features
+  'includes' => array('_aws'),
+  'services' => array(
+    // All AWS clients extend from 'default_settings'. Here we are
+    // overriding 'default_settings' with our default credentials and
+    // providing a default region setting.
+    'default_settings' => array(
+      'params' => array(
+	      'key'    => getenv('bigprimesawskey'),
+	    	'secret' => getenv('bigprimesawssecret'),
+        'region' => 'eu-west-1'
+      )
+    )
+  )
+);
