82e695ee6690437b74f3957211822f0d1e6f279e
craigmayhew <craig@mayhew.io>
2015-06-20 14:00:55 +0100

    Moved index.php to be inside web directory for .htaccess rules to work.

--- html/index.php
+++ html/index.php
@@ -2,15 +2,20 @@
 date_default_timezone_set('Europe/London');
 ini_set('memory_limit','64M');
 
+if(isset($argv[1])){
+  $_GET['page'] = $argv[1];
+}
+
 /*AUTOLOADER START*/
 //used to autoload classes
 function myAutoload($name){
     $doNotInclude = false;
-    if(file_exists($className = 'classes/'.str_replace('\\', '/', $name).'.php')){
+    $className = '../classes/'.str_replace('\\', '/', $name).'.php';
+    if(file_exists($className)){
     }else{
       header('HTTP/1.0 404 Not Found');
       $doNotInclude = true;
-    }   
+    }
     if($doNotInclude == false){
       require_once $className;
     }   
@@ -23,9 +28,7 @@ if(function_exists('spl_autoload_register')){
     }   
 }
 /*AUTOLOADER END*/
-
-require_once('vendor/autoload.php');
-
-$pageName = $_GET['page'];
+require_once('../vendor/autoload.php');
+$pageName = 'pages\\'.$_GET['page'];
 $page = new $pageName();
 echo $page->getContent();
