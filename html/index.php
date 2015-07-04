3c824a2fee378f30bc4ce06f26528440b96cd8c2
craigmayhew <craig@mayhew.io>
2015-07-04 20:23:57 +0000

    This looks important

---
+++ html/index.php
@@ -0,0 +1,35 @@
+<?php
+date_default_timezone_set('Europe/London');
+ini_set('memory_limit','256M');
+
+if(isset($argv[1])){
+  $_GET['page'] = $argv[1];
+}
+
+/*AUTOLOADER START*/
+//used to autoload classes
+function myAutoload($name){
+    $doNotInclude = false;
+    $className = '../classes/'.str_replace('\\', '/', $name).'.php';
+    if(file_exists($className)){
+    }else{
+      header('HTTP/1.0 404 Not Found');
+      $doNotInclude = true;
+    }
+    if($doNotInclude == false){
+      require_once $className;
+    }   
+}
+if(function_exists('spl_autoload_register')){
+    spl_autoload_register('myAutoload');
+}else{
+    function __autoload($name){
+        myAutoload($name);
+    }   
+}
+require_once('../vendor/autoload.php');
+/*AUTOLOADER END*/
+
+$pageName = 'pages\\'.$_GET['page'];
+$page = new $pageName();
+echo $page->getContent();
