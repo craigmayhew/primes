ab1513bafeb12fb56fcc42ce8f79314e30693c5d
craigmayhew <craig@mayhew.io>
2015-08-02 14:56:02 +0100

    Added notes, corrected logic.

--- classes/pages/genPrimeBlocks.php
+++ classes/pages/genPrimeBlocks.php
@@ -2,51 +2,55 @@
 namespace pages;
  
 class genPrimeBlocks extends \pages{
-  private $blockSize = 1001;//number of primes per block;
+  private $blockSize = 1000;//number of primes per block;
   public function getContent(){
+    //connect to mysql
     $conn = mysql_connect(getenv('bigprimesDBEndPoint'), getenv('bigprimesDBUser'), getenv('bigprimesDBPass'));
     mysql_select_db('bigprimes');
 
-    $q = 'SELECT `prime` FROM `prime_q` WHERE 1 AND `prime`!=1 ORDER BY `id` ASC LIMIT '.$this->blockSize;
+    //pull blockSize primes from the queue
+    $q = 'SELECT `prime` FROM `prime_q` WHERE `prime`!=1 ORDER BY `id` ASC LIMIT '.$this->blockSize;
     $res = mysql_query($q);
+    //if we don't find blockSize then we can't make a full block, so fail gracefully
     if(mysql_num_rows($res) != $this->blockSize){
        echo 'breaking loop.'.PHP_EOL;
        exit();
     }
 
-    $first = true;
+    //populate the halfDiffs array
     $prev = null;
     $halfDiffs = array();
     while($p = mysql_fetch_assoc($res)){
-      if(true === $first){
-        $n = $p['prime'];
-        $first = false;
-      }
       if($prev){
         $halfDiffs[] = bcdiv(bcsub($p['prime'],$prev),2);
+      }else{
+        $firstPrimeInBlock = $p['prime'];
       }
 
       $prev = $p['prime'];
     }
 
-    $largestDiff = max($halfDiffs);
+    $largestHalfDiff = max($halfDiffs);
 
+    //work out how many bits will be required to store the largest halfdiff
     $bits=2;
-    echo "ld: $largestDiff\r\n";
+    echo "ld: $largestHalfDiff\r\n";
     while (true){
-      if ((pow(2, $bits)) > $largestDiff){
+      //if we finally have enough bit as $bits^2 to store the largest halfDiff
+      if ((pow(2, $bits)) > $largestHalfDiff){
         echo "Choosing $bits bits\r\n";
         break;
       }
-      echo "Bits: $bits     " . (pow(2, $bits)) . "    $largestDiff\r\n";
+      echo "Bits: $bits     " . (pow(2, $bits)) . "    $largestHalfDiff\r\n";
       $bits++;
     }
   
     $temp = '';
     foreach ($halfDiffs as $halfDiff) {
-      $temp .= decbin($halfDiff);
+      // make all of the binary values be the same length so that we can extract them when we read the block
+      $temp .= str_pad(decbin($halfDiff), $bits, '0', STR_PAD_LEFT);
     }
-    echo $temp . "\r\n";
+
     $padding = 8 - (($bits * count($halfDiffs)) % 8);
     if ($padding == 8) $padding = 0;
     echo "Padding: $padding    Bits: $bits     count(): " . count($halfDiffs) . "\r\n";
@@ -61,8 +65,7 @@ class genPrimeBlocks extends \pages{
     echo $binary . "\r\n";
 
     $data = json_encode(array(
-              'n'=>$n,
-              'nth'=>$nth,
+              'n'=>$firstPrimeInBlock,
               'blockSize'=>$this->blockSize,
               'diffInBits'=>$bits
     ));
@@ -75,5 +78,7 @@ class genPrimeBlocks extends \pages{
     $s3FileName = 'ALLTHEPRIMES-'.$uuid;
     $aws->saveDataToS3($data,$s3FileName);
     $aws->saveDataToS3($binary,$s3FileName.'.bin');
+    echo "Have a look in $s3FileName, its very exciting!";
+    exit();
   }
 }
